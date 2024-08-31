4c40be5a72e89ad4e81d787a4c930a13b611d017
unknown <prasanjitprusty390@gmail.com>
2024-08-31 20:47:06 +0530

    inserted

--- src/app/store.js
+++
@@ -1,9 +0,0 @@
-// store.js
-import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from '../features/couter/counterSlice';
-
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
